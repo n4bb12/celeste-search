5a37ea6b780d3c2c8d0b28707fc7045308b1c3b1
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-12-14 20:01:58 +0100

    Generate responsive background images

---
+++ gulp/bg.js
@@ -0,0 +1,102 @@
+const { src, dest, series, parallel } = require("gulp")
+const { writeFile } = require("fs-extra")
+const del = require("del")
+const responsive = require("gulp-responsive")
+
+const ratio = 9 / 16
+const sourceWidth = 2048
+const paths = {
+  in: "assets/crete.png",
+  out: "generated/bg",
+}
+
+/**
+ * Determines the images sizes for `gulp-responsive`.
+ */
+function buildImagesConfigs(exponents) {
+  const images = []
+
+  exponents.forEach(exponent => {
+    const width = Math.round(sourceWidth * Math.pow(ratio, exponent - 1))
+    const height = Math.round(sourceWidth * Math.pow(ratio, exponent))
+    const size = height
+
+    images.push({
+      rename: `bg-${width}-${height}.png`,
+      width,
+      height,
+    })
+    images.push({
+      rename: `bg-${size}.png`,
+      width: size,
+      height: size,
+    })
+  })
+
+  return images
+}
+
+/**
+ * Builds a CSS string containing media queries for the different
+ * image sizes.
+ */
+const buildCSS = images => {
+  const rules = []
+
+  function addRule(...lines) {
+    rules.push("  " + lines.map(line => line.trim()).join(" "))
+  }
+
+  images.forEach(({ rename, width, height }) => {
+
+    const backgroundImage
+      = `background-image: url("/assets/generated/bg/${rename}");`
+
+    if (!rules.length) {
+      addRule(backgroundImage)
+    } else if (width !== height) {
+      addRule(
+        `@media (max-width: ${width}px), (max-height: ${height}px) {`,
+        `  ${backgroundImage}`,
+        `}`)
+    } else {
+      addRule(
+        `@media (max-aspect-ratio: 1/1) and (max-height: ${height}px) {`,
+        `  ${backgroundImage}`,
+        `}`)
+    }
+  })
+
+  return `body.--stable {
+${rules.join("\n")}
+}
+`
+}
+
+/**
+ * Deletes all current backgrounds.
+ */
+const deleteBGs = () => del(paths.out + "/*")
+
+/**
+ * Generates multiple sizes of background images.
+ */
+const generateBGs = done => {
+  const images = buildImagesConfigs([1, 1.25, 1.5, 2, 3])
+
+  const writeImages = () => src(paths.in)
+    .pipe(responsive({ "*": images }))
+    .pipe(dest(paths.out))
+
+  const writeCSS = () => {
+    const css = buildCSS(images)
+    return writeFile(paths.out + "/bg.css", css, "utf8")
+  }
+
+  return parallel(writeCSS, writeImages)(done)
+}
+
+/**
+ * `gulp bg`
+ */
+module.exports = series(deleteBGs, generateBGs)
--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,3 @@
+exports.bg = require("./gulp/bg")
 exports.optimize = require("./gulp/optimize")
 exports.sprites = require("./gulp/sprites")
