99a55a1900d5cac85b392ff989dabd7a34bb9ce4
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-12-20 04:20:17 +0100

    Fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,35 @@
 import { HttpClientModule } from "@angular/common/http"
 import { async, TestBed } from "@angular/core/testing"
 import { ReactiveFormsModule } from "@angular/forms"
-import { BrowserModule } from "@angular/platform-browser"
+
+import { NgScrollbarModule } from "ngx-scrollbar"
 
 import { AppComponent } from "./app.component"
 import { AwaitFontDirective } from "./await-font.directive"
 import { BackToTopComponent } from "./back-to-top/back-to-top.component"
+import { ComparisonComponent } from "./comparison/comparison.component"
 import { LegalComponent } from "./legal/legal.component"
 import { ResultsComponent } from "./results/results.component"
 import { SearchComponent } from "./search/search.component"
+import { TabsComponent } from "./tabs/tabs.component"
 
 describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        BrowserModule,
         HttpClientModule,
+        NgScrollbarModule,
         ReactiveFormsModule,
       ],
       declarations: [
         AppComponent,
         AwaitFontDirective,
         BackToTopComponent,
+        ComparisonComponent,
         LegalComponent,
         ResultsComponent,
         SearchComponent,
+        TabsComponent,
       ],
     }).compileComponents()
   }))
