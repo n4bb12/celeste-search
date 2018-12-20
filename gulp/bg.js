e92c872d69182b7a45a7d8b798b46e6fcb93ecbe
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-12-20 17:31:34 +0100

    Use absolute URLs in CSS

--- gulp/bg.js
+++ gulp/bg.js
@@ -6,7 +6,7 @@ const responsive = require("gulp-responsive")
 const ratio = 9 / 16
 const sourceWidth = 2048
 const paths = {
-  in: "assets/norse.png",
+  in: "/assets/norse.png",
   out: "generated/bg",
 }
 
