69f7ee944101eae4f449fe7c737a05560ddf5c98
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-12-21 16:05:11 +0100

    Improve responsive padding

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -3,7 +3,6 @@ import { ChangeDetectionStrategy, Component, Input } from "@angular/core"
 import { map } from "rxjs/operators"
 
 import { SearchService } from "../services"
-import { TABS } from "../tabs/tabs"
 
 @Component({
   selector: "cis-results",
@@ -15,9 +14,8 @@ export class ResultsComponent {
 
   @Input() activeTab = 0
 
-  readonly tabs = [...TABS]
-  readonly items = this.search.items.pipe(map(results => results[0]))
-  readonly advisors = this.search.advisors.pipe(map(results => results[0]))
+  readonly items = this.search.items.pipe(map(results => results.slice(0, 10)))
+  readonly advisors = this.search.advisors.pipe(map(results => results.slice(0, 10)))
 
   constructor(
     private search: SearchService,
