be6b9a127cfa2967da426c189964853041feaaf3
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-13 23:14:17 +0200

    Add fallback text parameter

--- db/shared/convert-text.ts
+++ db/shared/convert-text.ts
@@ -1,18 +1,21 @@
+import chalk from "chalk"
+
 import { API } from "../download"
 
 /**
  * Returns an english translation from the API.
  */
-export async function translateEn(id: number): Promise<string> {
+export async function translateEn(id: number, fallback?: string): Promise<string> {
   const languages = await API.getLanguages()
 
   const translation =
     languages.stringtablex.language.English.string[id] ||
     languages.econstrings.language.English.string[id]
 
-  if (!translation) {
-    throw new Error("No translation found for " + id)
+  if (translation && translation.text) {
+    return translation.text
   }
 
-  return translation.text
+  console.log(chalk.yellow(`No translation found for ${id}, falling back to ${fallback}`))
+  return fallback
 }
