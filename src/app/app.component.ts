d2591c33254c9317da5249a07c39f68071faef5c
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-18 22:53:25 +0200

    Cleanup app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,8 @@
 import { Component } from "@angular/core"
 
-import { DbService } from "./services/db.service"
-
 @Component({
   selector: "cis-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-
-  activeTab = 0
-  readonly items = this.db.fetch()
-
-  constructor(
-    private db: DbService,
-  ) { }
-
-}
+export class AppComponent { }
