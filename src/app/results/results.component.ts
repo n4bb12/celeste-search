23c22f0ded00503405adc6865cc1e56c40c72d57
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-19 22:10:54 +0200

    Remove result limit

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,7 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from "@angular/core"
 
-import { map } from "rxjs/operators"
-
 import { SearchService, TabService } from "../services"
 import { Item } from "./../interfaces/Item"
 
@@ -15,11 +13,11 @@ export class ResultsComponent implements OnInit {
 
   readonly activeTabChange = this.tabs.changes
 
-  readonly items = this.search.items.pipe(map(results => results.slice(0, 50)))
-  readonly advisors = this.search.advisors.pipe(map(results => results.slice(0, 50)))
-  readonly blueprints = this.search.blueprints.pipe(map(results => results.slice(0, 50)))
-  readonly designs = this.search.designs.pipe(map(results => results.slice(0, 50)))
-  readonly consumables = this.search.consumables.pipe(map(results => results.slice(0, 50)))
+  readonly items = this.search.items
+  readonly advisors = this.search.advisors
+  readonly blueprints = this.search.blueprints
+  readonly designs = this.search.designs
+  readonly consumables = this.search.consumables
 
   numColumns = 3
 
