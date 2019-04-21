5d9857af3b98c1d56f4d034d46657665bed5be0b
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-21 17:51:38 +0200

    Add weapon, armor and vanity tag

--- db/items/search.ts
+++ db/items/search.ts
@@ -117,6 +117,58 @@ export async function buildSearchString(item: Item, trait: Trait): Promise<strin
     builder.add("Celeste")
   }
 
+  if ([
+    "BallistaArms",
+    "BellyBows",
+    "Bows",
+    "Clubs2H",
+    "FireThrowers",
+    "GreatAxe",
+    "Javalins",
+    "RamHeads",
+    "Sling",
+    "Spears1H",
+    "Spears2H",
+    "Swords1H",
+  ].includes(trait.traittype)) {
+    builder.add("weapon")
+  }
+
+  if ([
+    "ArmorBuilding",
+    "ArmorCloth",
+    "ArmorLgt",
+    "ArmorMed",
+    "ArmorPlt",
+    "Arrows",
+    "FishingNet",
+    "Gear",
+    "GearBldg",
+    "GearBoat",
+    "GearPriest",
+    "GearSiege",
+    "GearVill",
+    "Merchant",
+    "Scepter",
+    "ScoutSpecial1H",
+    "Shields",
+    "Staffs2H",
+    "Tools",
+    "Torc",
+    "War Horn",
+    "Warpaint",
+  ].includes(trait.traittype)) {
+    builder.add("armor")
+  }
+
+  if ([
+    "VanityHelm",
+    "VanityShield",
+    "VanityWeapon",
+  ].includes(trait.traittype)) {
+    builder.add("vanity")
+  }
+
   return builder.build()
 }
 
