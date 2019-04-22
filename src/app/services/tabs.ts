4db510045dabf20aff892d3f1ecadb3116c200fe
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-22 20:08:57 +0200

    Update advisor icon

--- src/app/services/tabs.ts
+++ src/app/services/tabs.ts
@@ -1,7 +1,7 @@
 export const TABS = [
-  { id: "items", name: "Items", icon: "GreekTechSpearmanS_ua" },
-  { id: "advisors", name: "Advisors", icon: "HerbalRemedies2" },
-  { id: "blueprints", name: "Blueprints", icon: "TechTower3_ua" },
   // { id: "designs", name: "Designs", icon: "Trading_Contract_2" },
   // { id: "consumables", name: "Consumables", icon: "C06TechPharmacology_ua" },
+  { id: "items",       name: "Items",       icon: "GreekTechSpearmanS_ua" },
+  { id: "advisors",    name: "Advisors",    icon: "HerbalRemedies5"       },
+  { id: "blueprints",  name: "Blueprints",  icon: "TechTower3_ua"         },
 ]
