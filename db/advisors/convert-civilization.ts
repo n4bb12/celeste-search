771082d919ac355b55d5b7ed39ed14c9cab1b048
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-26 20:48:30 +0200

    Fix advisor civ mapping

--- db/advisors/convert-civilization.ts
+++ db/advisors/convert-civilization.ts
@@ -1,12 +1,5 @@
-const displayNames = {
-  Egypt: "Egyptian",
-  Greek: "Greek",
-  Celt: "Celtic",
-  Persia: "Persian",
-  Norse: "Norse",
-  Babylonian: "Babylonian",
-}
-
-export function convertCivilization(civilization?: string): string {
-  return displayNames[civilization || ""]
+export function convertCivilization(civilization: string | undefined) {
+  if (civilization) {
+    return civilization.substr("eCivMatchingType".length).toLowerCase()
+  }
 }
