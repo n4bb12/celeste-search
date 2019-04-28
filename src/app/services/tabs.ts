0f1ff2a7dd406c9f2af29c5a1c2ab35966a83302
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-04-28 02:04:50 +0200

    Add designs tab

--- src/app/services/tabs.ts
+++ src/app/services/tabs.ts
@@ -2,6 +2,6 @@ export const TABS = [
   { id: "items",       name: "Items",       icon: "GreekTechSpearmanS_ua",  visible: true  },
   { id: "advisors",    name: "Advisors",    icon: "HerbalRemedies5",        visible: true  },
   { id: "blueprints",  name: "Blueprints",  icon: "TechTower3_ua",          visible: true  },
-  { id: "designs",     name: "Designs",     icon: "Trading_Contract_2",     visible: false },
+  { id: "designs",     name: "Recipes",     icon: "Trading_Contract_2",     visible: true  },
   { id: "consumables", name: "Consumables", icon: "C06TechPharmacology_ua", visible: false },
 ]
