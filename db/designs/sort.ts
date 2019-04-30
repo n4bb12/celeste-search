a8b83aadb0eb6be095eb03779b867c6589ba6827
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-05-01 00:58:38 +0200

    Sort results

--- db/designs/sort.ts
+++ db/designs/sort.ts
@@ -1,5 +1,14 @@
-import { Design } from "../interfaces"
+import { Design, RARITIES } from "../interfaces"
 
 export function compareDesigns(a: Design, b: Design) {
+  const aRarity = a.rarity
+  const bRarity = b.rarity
+
+  if (aRarity !== bRarity) {
+    const aRarityIndex = RARITIES.indexOf(aRarity)
+    const bRarityIndex = RARITIES.indexOf(bRarity)
+    return aRarityIndex - bRarityIndex
+  }
+
   return a.outputName.localeCompare(b.outputName)
 }
