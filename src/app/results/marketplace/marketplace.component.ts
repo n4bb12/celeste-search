d8cf37d5286ff2b5a8cc02c56f1b70bd402d93fe
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-05-01 20:14:14 +0200

    Move marketplace fetching into marketplace component

--- src/app/results/marketplace/marketplace.component.ts
+++ src/app/results/marketplace/marketplace.component.ts
@@ -1,6 +1,18 @@
-import { ChangeDetectionStrategy, Component, Input } from "@angular/core"
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+} from "@angular/core"
 
 import { MarketplaceItem } from "celeste-api-types"
+import { isEqual } from "lodash"
+import { Subscription } from "rxjs"
+import { distinctUntilChanged, map, tap } from "rxjs/operators"
+
+import { DbService } from "../../services"
 
 @Component({
   selector: "cis-marketplace",
@@ -8,9 +20,38 @@ import { MarketplaceItem } from "celeste-api-types"
   styleUrls: ["./marketplace.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MarketplaceComponent {
+export class MarketplaceComponent implements OnInit, OnDestroy {
 
-  @Input() marketplace: MarketplaceItem[]
+  @Input() id: string
+
+  marketplace: MarketplaceItem[]
+
+  private subscriptions: Subscription[] = []
+
+  constructor(
+    private changeRef: ChangeDetectorRef,
+    private db: DbService,
+  ) { }
+
+  ngOnInit() {
+    const byPriceAsc = (a: MarketplaceItem, b: MarketplaceItem) => {
+      return a.ItemPrice / a.ItemCount - b.ItemPrice / b.ItemCount
+    }
+
+    const marketplaceSub = this.db.marketplace.pipe(
+      map(market => market.data.filter(entry => entry.ItemID === this.id)),
+      distinctUntilChanged(isEqual),
+      map(market => market.sort(byPriceAsc)),
+      tap(market => this.marketplace = market),
+      tap(() => this.changeRef.detectChanges()),
+    ).subscribe()
+
+    this.subscriptions.push(marketplaceSub)
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe())
+  }
 
   getPriceRange() {
     const lowestInt = this.marketplace[0].ItemPrice
--- src/app/results/shell/shell.component.ts
+++ src/app/results/shell/shell.component.ts
@@ -1,19 +1,6 @@
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  Input,
-  OnDestroy,
-  OnInit,
-} from "@angular/core"
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core"
 
-import { MarketplaceItem } from "celeste-api-types"
-import { isEqual } from "lodash"
-import { Subscription } from "rxjs"
-import { distinctUntilChanged, map, tap } from "rxjs/operators"
-
-import { Blueprint, Item, Materials } from "../../interfaces"
-import { DbService } from "../../services"
+import { Blueprint, Item } from "../../interfaces"
 
 @Component({
   selector: "cis-shell",
@@ -21,7 +8,7 @@ import { DbService } from "../../services"
   styleUrls: ["./shell.component.scss"],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ShellComponent implements OnInit, OnDestroy {
+export class ShellComponent {
 
   @Input() id: Item["id"]
   @Input() name: Item["name"]
@@ -33,40 +20,5 @@ export class ShellComponent implements OnInit, OnDestroy {
   @Input() vendors: Item["vendors"]
   @Input() levels?: number[]
   @Input() rarities?: string[]
-
-  materials: Materials
-  marketplace: MarketplaceItem[]
-
-  private subscriptions: Subscription[] = []
-
-  constructor(
-    private changeRef: ChangeDetectorRef,
-    private db: DbService,
-  ) { }
-
-  ngOnInit() {
-    const materialsSub = this.db.shared.subscribe(db => {
-      this.materials = db.materials
-      this.changeRef.detectChanges()
-    })
-
-    const byPrice = (a: MarketplaceItem, b: MarketplaceItem) => {
-      return a.ItemPrice / a.ItemCount - b.ItemPrice / b.ItemCount
-    }
-
-    const marketplaceSub = this.db.marketplace.pipe(
-      map(market => market.data.filter(entry => entry.ItemID === this.id)),
-      distinctUntilChanged(isEqual),
-      map(market => market.sort(byPrice)),
-      tap(market => this.marketplace = market),
-      tap(() => this.changeRef.detectChanges()),
-    ).subscribe()
-
-    this.subscriptions.push(materialsSub, marketplaceSub)
-  }
-
-  ngOnDestroy() {
-    this.subscriptions.forEach(sub => sub.unsubscribe())
-  }
 
 }
