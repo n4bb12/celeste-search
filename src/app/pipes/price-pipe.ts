ee97d42b8d2b2c1fcfc9a7b36d39de4a49c4827a
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-05-04 01:14:29 +0200

    Only show one decimal places from 1 to 9 of each price unit

--- src/app/pipes/price-pipe.ts
+++ src/app/pipes/price-pipe.ts
@@ -6,11 +6,20 @@ import { Pipe, PipeTransform } from "@angular/core"
 export class PricePipe implements PipeTransform {
 
   transform(price: number): string {
+    if (price >= 1000 * 1000 * 10) {
+      return (price / 1000 / 1000).toFixed(0) + "M"
+    }
     if (price >= 1000 * 1000) {
-      return (price / 1000 / 1000).toFixed(1) +  "M"
+      return (price / 1000 / 1000).toFixed(1).replace(/.0$/, "") + "M"
+    }
+    if (price >= 1000 * 10) {
+      return (price / 1000).toFixed(0) + "k"
     }
     if (price >= 1000) {
-      return (price / 1000).toFixed(0) +  "k"
+      return (price / 1000).toFixed(1).replace(/.0$/, "") + "k"
+    }
+    if (price >= 1000) {
+      return (price / 1000).toFixed(0) + "k"
     }
     return price.toFixed(0)
   }
