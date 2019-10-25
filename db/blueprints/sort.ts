1b9e3c69f4dd2ef2294d21120ee0495373ee1a4f
Abraham Schilling <AbrahamSchilling@gmail.com>
2019-10-25 19:39:25 +0200

    Improve stability of the blueprint sort order

--- db/blueprints/sort.ts
+++ db/blueprints/sort.ts
@@ -1,14 +1,13 @@
 import { Blueprint, RARITIES } from "../interfaces"
 
 export function compareBlueprints(a: Blueprint, b: Blueprint) {
-  const aRarity = a.rarity
-  const bRarity = b.rarity
-
-  if (aRarity !== bRarity) {
-    const aRarityIndex = RARITIES.indexOf(aRarity)
-    const bRarityIndex = RARITIES.indexOf(bRarity)
+  if (a.rarity !== b.rarity) {
+    const aRarityIndex = RARITIES.indexOf(a.rarity)
+    const bRarityIndex = RARITIES.indexOf(b.rarity)
     return aRarityIndex - bRarityIndex
   }
-
-  return a.name.localeCompare(b.name)
+  if (a.name !== b.name) {
+    return a.name.localeCompare(b.name)
+  }
+  return a.id.localeCompare(b.id)
 }
